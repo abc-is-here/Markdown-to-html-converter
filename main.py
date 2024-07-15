45f4ccb83f01dc539fd0462f4da81be3f365cae1
abc <abhaygupta.hsj@gmail.com>
2024-07-15 18:26:09 +0530

    Added new features!

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import re
 
 def parse_markdown_line(line):
-
     header_match = re.match(r'^(#{1,6})\s*(.+)', line)
     if header_match:
         header_level = len(header_match.group(1))
         header_content = header_match.group(2)
         return f'<h{header_level}>{header_content}</h{header_level}>'
-    
-    return f'<p>{line}</p>'
+
+    if line.startswith('- '):
+        return f'<li>{line[2:]}</li>'
+
+    bold_text = re.sub(r'\*\*(.+?)\*\*', r'<strong>\1</strong>', line)
+
+
+    italic_text = re.sub(r'\*(.+?)\*', r'<em>\1</em>', bold_text)
+
+    return f'<p>{italic_text}</p>'
 
 def convert_markdown_to_html(markdown_text):
+
     lines = markdown_text.split('\n')
-    html_lines = [parse_markdown_line(line) for line in lines]
+    html_lines = []
+
+    in_list = False
+    for line in lines:
+        if line.startswith('- '):
+            if not in_list:
+                html_lines.append('<ul>')
+                in_list = True
+            html_lines.append(parse_markdown_line(line))
+        else:
+            if in_list:
+                html_lines.append('</ul>')
+                in_list = False
+            html_lines.append(parse_markdown_line(line))
+
+    if in_list:
+        html_lines.append('</ul>')
+
     return '\n'.join(html_lines)
 
 markdown_text = """
 # Header 1
 ## Header 2
-This is a paragraph.
+This is a **bold** paragraph with *italic* text.
+- Item 1
+- Item 2
+- Item 3
 """
 
 html_output = convert_markdown_to_html(markdown_text)
